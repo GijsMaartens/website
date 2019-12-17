6d0b1019cbcbf97e66e19b93d32ba6c1957c6997
Tim Logtenberg <tplogtenberg@gmail.com>
2019-12-17 13:14:28 +0100

    Nuget package downgrade

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace SchoolTemplate.Controllers
   public class HomeController : Controller
   {
     // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=;Uid=;Pwd=;";
+    string connectionString = "Server=172.16.160.21;Port=3306;Database=fastfood;Uid=lgg;Pwd=LifeUniverseEverything;";
 
     public IActionResult Index()
     {
@@ -35,14 +35,21 @@ namespace SchoolTemplate.Controllers
         {
           while (reader.Read())
           {
+            int Id = Convert.ToInt32(reader["Id"]);
+            string Naam = reader["Naam"].ToString();
+            float Calorieen = float.Parse(reader["calorieen"].ToString());
+            string Formaat = reader["Formaat"].ToString();
+            int Gewicht = Convert.ToInt32(reader["Gewicht"].ToString());
+            decimal Prijs = Decimal.Parse(reader["Prijs"].ToString());
+
             Product p = new Product
             {
               Id = Convert.ToInt32(reader["Id"]),
               Naam = reader["Naam"].ToString(),
               Calorieen = float.Parse(reader["calorieen"].ToString()),
-              Formaat = reader["Naam"].ToString(),
-              Gewicht = Convert.ToInt32(reader["Naam"].ToString()),
-              Prijs = Decimal.Parse(reader["Naam"].ToString())
+              Formaat = reader["Formaat"].ToString(),
+              Gewicht = Convert.ToInt32(reader["Gewicht"].ToString()),
+              Prijs = Decimal.Parse(reader["Prijs"].ToString())
             };
             products.Add(p);
           }
