3e64c2d0091ad15e281ef0e00ad158e160b16c25
GilbertGobbels <110086@st-maartenscollege.nl>
2021-04-29 21:42:00 +0200

    Build basic data structures

---
+++ SchoolTemplate/Database/Evenement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SchoolTemplate.Database
+{
+  public class Evenement
+  {
+    public int Id { get; set; }
+    
+    public string Naam { get; set; }
+
+    public string Beschrijving { get; set; }
+
+    public string Locatie { get; set; }
+
+    public string Logo { get; set; }
+
+    public Decimal Prijs { get; set; }
+
+    public DateTime Datum { get; set; }
+  }
+}
---
+++ SchoolTemplate/Models/ContactBericht.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolTemplate.Models
+{
+    public class ContactBerichtModel
+    {
+
+        [Required(ErrorMessage = "Een emailadres is verplicht.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Een naam is verplicht.")]
+        public string Naam { get; set; }
+
+        [Required(ErrorMessage = "Een geboortedatum is verplicht.")]
+        public DateTime Geboortedatum { get; set; }
+
+        [Required(ErrorMessage = "Een huisnummer is verplicht.")]
+        public int Huisnummer { get; set; }
+
+        [Required(ErrorMessage = "Een postcode is verplicht.")]
+        public string Postcode { get; set; }
+
+        [Required(ErrorMessage = "Een bericht is verplicht.")]
+        public string Bericht { get; set; }
+    }
+}
